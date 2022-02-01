91b039e7bbc7153038052ccc2b77f79ebb5b4f08
mastanpatel <Bushra.mateen95@outlook.com>
2022-02-01 17:35:00 +0530

    map styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import  BingMapsReact from "bingmaps-react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const MapContainer = () => {
+  const [map,setMap] = useState()
+  
+  const mapStyles = {
+    height: "25px",
+    weight : "25px"
+    
+  };
+  const infobox = () => {
+    const center = {title: 'Map Center', description: 'Seattle', visible: false}
+  }
+  // const defaultCenter = {
+  //   lat: 41.3851, lng:2.1734
+  // }
+
+  // var map = new window.Microsoft.Maps.Map(document.getElementById('myMap'), {});
+  // var pushpin = new window.Microsoft.Maps.Pushpin(map.getCenter(), null);
+  // map.entities.push(pushpin);
+  const pushPin = {
+    center: {
+      latitude: 17.3297,
+      longitude:76.8343,
+    },
+    options: {
+      title: "Kalaburgi",
+    },
+  }
+  
+  const pushPins = [pushPin];
+  
+ 
+  return(
+    <div className='App'>
+      <div className='App-header'>
+      <div className='mapView'> 
+            <div className='map-container'>
+               <BingMapsReact
+                bingMapsKey = "AuuFKrGBGetYti2EVYWg9Qt1OVhag1QTSJkyREwMyKDAsqEJSShkxHKaahe9WlDc"
+                MapContainerStyle={mapStyles}
+                zoom={13}
+                // center = {defaultCenter}  
+                pushPins={pushPins}
+                viewOptions={{ center: { latitude: 17.3297, longitude: 76.8343} }}
+                /> 
+            </div>
+            <div className='sidebar'>
+                sidebar
+            </div>
+          </div>
+      </div>
+      
+    </div>
+    
+    
+    
+  )
+}
+export default MapContainer
